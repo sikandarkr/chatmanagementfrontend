fcfc4acbe4f20e821c528d5766cf879956e736ae
Sikandar <sikandar@edgecorporation.net>
2022-09-30 01:16:25 +0530

    added folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
+import "antd/dist/antd.css";
+import { TimePicker } from 'antd';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{
+      display: 'block', width: 700, padding: 30
+      }}>
+      <h4>ReactJS Ant-Design TimePicker Component</h4>
+      <>
+          <TimePicker onChange={(time) => console.log(time)} />,
+      </>
+      </div>
   );
 }
 
