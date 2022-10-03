a40ae512e185a01c8bda45649fb22ed1cbc9b9b8
sikandarkr <49118531+sikandarkr@users.noreply.github.com>
2022-10-03 16:43:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import { Routes, Route ,BrowserRouter as Router} from "react-router-dom";
+
+import { Routes, Route, Navigate, useNavigate, useLocation ,BrowserRouter as Router} from 'react-router-dom';
 import Home from '../src/Pages/Home';
 import Login from '../src/Pages/Login';
 import Register from "./Pages/Register";
 import PrivateRoute from '../src/Routes/PrivateRoute';
 import PublicRoute from "./Routes/PublicRoute";
+
 import "antd/dist/antd.css";
-
-import { TimePicker } from 'antd';
-import './App.css';
-
 function App() {
- return  <div className="App">
-          <Router>
-              <Routes>
-                  {/* <Route path="/home" element={<Home/>}>
-                  </Route> */}
-                 
-                     <Route exact path="/login" element={<PublicRoute><Login /></PublicRoute>} />
-                     <Route exact path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
-                 
-                  {/* <PublicRoute restricted={false} element={<Login/>} path="/login" exact /> */}
-                  {/* <Route path="/login" element={<Login />}>
-                  </Route> */}
-                  {/* <PrivateRoute element={<Home />} path="/home" exact /> */}
-                  {/* <Route path="/register" element={<Register />}>
-                  </Route> */}
-              </Routes>
-          </Router>
-    </div>
+    // init custom history object to allow navigation from 
+    // anywhere in the react app (inside or outside components)
+    // history.navigate = useNavigate();
+    // history.location = useLocation();
+
+    return (
+        <div className="app-container bg-light">
+            <div className="container pt-4 pb-4">
+                <Router>
+                    <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route
+                        path="/"
+                        element={
+                            <PrivateRoute>
+                                <Home />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route path="*" element={<Navigate to="/" />} />
+                    </Routes>
+                </Router>
+            </div>
+        </div>
+    );
 }
 
 export default App;
-
-
-{/* <PublicRoute restricted={false} component={Home} path="/" exact />
-<PublicRoute restricted={true} component={SignIn} path="/signin" exact />
-<PrivateRoute component={Dashboard} path="/dashboard" exact /> */}
