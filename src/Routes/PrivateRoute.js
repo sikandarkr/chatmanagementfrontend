493f93c76af25481ac042541ab0b6929d887e9fb
sikandarkr <49118531+sikandarkr@users.noreply.github.com>
2022-10-03 16:45:59 +0530

    Update PrivateRoute.js

--- src/Routes/PrivateRoute.js
+++ src/Routes/PrivateRoute.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Route,Navigate } from 'react-router-dom'
 import {isLoggedIn} from '../Utils/utils'
-const PrivateRoute = ({ path, ...props }) => {
-    // const { loginStatus } = useAuth();
-  
-    return isLoggedIn ? (
-      <Route {...props} path={path} />
-    ) : (
-      <Navigate to="/login" state={{ from: path }} replace />
-    );
-  };
-  export default PrivateRoute;
+import { Navigate } from 'react-router-dom';
+function PrivateRoute({ children }) {
+    if (!isLoggedIn) {
+        return <Navigate to="/login"  />
+    }
+    return children;
+}
+
+export default PrivateRoute;
