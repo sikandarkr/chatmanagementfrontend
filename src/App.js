ab284db68d86551f7c89250b759414e451589904
rahul <royalrahul147@gmail.com>
2022-10-03 23:33:33 +0530

    adding top-seach-bar in the project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import logo from './logo.svg';
 import "antd/dist/antd.css";
 import { TimePicker } from 'antd';
+import SearchBar from  './Component/common/SearchBar'
+
 import './App.css';
 
 function App() {
   return (
-    <div style={{
-      display: 'block', width: 700, padding: 30
-      }}>
-      <h4>ReactJS Ant-Design TimePicker Component</h4>
-      <>
-          <TimePicker onChange={(time) => console.log(time)} />,
-      </>
-      </div>
+    <div> 
+    <SearchBar/>
+    </div>
+      
+     
   );
 }
 
---
+++ src/Component/common/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+function SearchBar(){
+
+        
+    
+
+    return(
+        <div className = "search-bar">
+        <input type="text" placeholder="Search type" className="search-field"/>
+
+
+
+       </div>
+                      
+    )
+
+}
+export default SearchBar
